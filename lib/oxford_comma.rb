4aa23b60d10159705456d8b266bbf9531d19e0bf
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-02-05 08:06:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,15 @@
 require 'pry'
 
-def oxford_comma(array)
-  if array.length == 2
-    "#{array[0]} and #{array[1]}"
-  elsif array.length > 2
-    final_word = array.pop
-    array.join(", ") << ", and #{final_word}"
-  else 
-    array[0]
-  end
-end
+# def oxford_comma(array)
+#   if array.length == 2
+#     "#{array[0]} and #{array[1]}"
+#   elsif array.length > 2
+#     final_word = array.pop
+#     array.join(", ") << ", and #{final_word}"
+#   else 
+#     array[0]
+#   end
+# end
 
 # def oxford_comma(array)
 #   if array.length == 2
@@ -19,3 +19,4 @@ end
 #   end
 #   array.join(", ")    
 # end
+
