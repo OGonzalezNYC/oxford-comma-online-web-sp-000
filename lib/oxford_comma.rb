c0965d3248f65a9d6c736be09d1c449d3c2b2e88
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-02-05 08:13:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -21,6 +21,10 @@ require 'pry'
 # end
 
 def oxford_comma(array)
+  if array.size == 1 
+    array[0]
+  elsif array.size == 2 
+    array.join(" and ")
   final_element = array.last
   array.pop
   initial_string = array.join(", ")
